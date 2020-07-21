78e139514ab5145f1d0a78983698c758b1612036
Kaspars <m3g4fix@gmail.com>
2020-07-21 12:19:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+// const result = confirm("Tu te?");
+// console.log(result);
+
+// const answer = prompt(" 18? ", "");
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt ('was?','');
+// answers[1] = prompt ('wo?','');
+// answers[2] = prompt ('warum?','');
+
+
+console.log(typeof(answers));
+
+const user = 'Ivan';
+
+alert(`hello, ${user}`);
