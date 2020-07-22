83a98bbdd525a2195792c6d8d5e12ef2d17261c2
Kaspars <m3g4fix@gmail.com>
2020-07-22 16:32:39 +0300

    job 4

--- js/script.js
+++ js/script.js
@@ -1,66 +1,71 @@
-let numberOfFilms;
 let personalMovieDb = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
+    start: function () {
+        personalMovieDb.count = +prompt('How many movies have you watched?', '');
+        while (personalMovieDb.count == '' ||
+            personalMovieDb.count == null || isNaN(personalMovieDb.count)) {
+            personalMovieDb.count = +prompt('How many movies have you watched?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const latestFilm1 = prompt('What film have you watched?', ''),
+                latestScore1 = prompt('What would you score it?', '');
+
+            if (latestFilm1 != null && latestScore1 != null &&
+                latestFilm1 != '' && latestScore1 != '' && latestFilm1.length < 50) {
+                personalMovieDb.movies[latestFilm1] = latestScore1;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDb.count < 10) {
+            console.log('few');
+        } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+            console.log('Okai');
+        } else if (personalMovieDb.count >= 30) {
+            console.log('dofiga');
+        } else {
+            console.log('error');
+        }
+    },
+    showMyDB: function () {
+        if (!personalMovieDb.private) {
+            console.log(personalMovieDb);
+        }
+    },
+
+    writeYourGenres: function () {
+        for (let i = 0; i < 3; i++) {
+            let genre = prompt(`your favourite genre no. ${i+1}`, '');
+            if (genre == null ||
+                genre == '') {
+                i--;
+            } else {
+                personalMovieDb.genres[i] = genre;
+            }
+        }
+        personalMovieDb.genres.forEach((value, index) => {
+            console.log(`Favourite genre #${index+1} is ${value}`);
+        });
+    },
+
+    toggleVisibleMyDB: function () {
+        personalMovieDb.private = !personalMovieDb.private;
+    }
 };
 
 
-// start();
-
-// rememberMyFilms();
-
-// detectPersonalLevel();
-writeYourGenres();
-showMyDB();
-
-console.log(personalMovieDb);
-
-function start() {
-    numberOfFilms = +prompt('How many movies have you watched?', '');
-
-    while (numberOfFilms == '' || 
-    numberOfFilms == null || isNaN(numberOfFilms) ) {
-        numberOfFilms = +prompt('How many movies have you watched?', '');
-    }
-}
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++){
-        const latestFilm1 = prompt('What film have you watched?', ''),
-                latestScore1 = prompt('What would you score it?', '');
-
-        if ( latestFilm1 != null && latestScore1 != null && 
-            latestFilm1 != '' && latestScore1 !='' && latestFilm1.length <50){
-            personalMovieDb.movies[latestFilm1] = latestScore1;
-        } else {
-            i--;
-        }    
-    }
-}
-
-function detectPersonalLevel(){
-    if (personalMovieDb.count<10){
-        console.log('few');
-    } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
-        console.log('Okai');
-    } else if (personalMovieDb.count >= 30) {
-        console.log('dofiga');
-    } else {
-        console.log('error');
-    }
-}
-
-function showMyDB(hidden) {
-    if(!hidden) {
-        console.log(personalMovieDb);
-    }
-}
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++){
-        personalMovieDb.genres[i] = prompt(`your favourite genre no. ${i+1}`, '');
-    }
-}
+// personalMovieDb.start();
+// personalMovieDb.rememberMyFilms();
+// personalMovieDb.detectPersonalLevel();
+personalMovieDb.writeYourGenres();
+personalMovieDb.showMyDB();
+personalMovieDb.toggleVisibleMyDB();
+personalMovieDb.showMyDB();
